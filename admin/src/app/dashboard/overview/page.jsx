7069a1a38c552967be377a20c7542c8722a08440
viyaga2022 <mohan2609kumar@gmail.com>
2024-12-29 09:03:25 +0530

    admin moved

--- admin/src/app/dashboard/overview/page.jsx
+++
@@ -1,9 +0,0 @@
-import OverViewPage from "@/components/overview";
-
-export const metadata = {
-  title: 'Dashboard : Overview'
-};
-
-export default function page() {
-  return <OverViewPage />;
-}
