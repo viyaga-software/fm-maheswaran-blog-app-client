d0942d4512436e83086bd12fc1a86bb215a07001
viyaga-software <viyagaenterprises@gmail.com>
2025-03-23 00:44:36 +0530

    Update FeaturedPostCard.jsx

--- src/ui/pages/home/components/featuredPosts/FeaturedPostCard.jsx
+++ src/ui/pages/home/components/featuredPosts/FeaturedPostCard.jsx
@@ -7,25 +7,24 @@ import { Badge } from "@/components/ui/badge";
 const FeaturedPostCard = ({ post, sNo }) => {
   return (
     <Link href={`/${post.slug}`}>
-      <Card className="flex gap-6 p-5 shadow-lg border rounded-xl hover:shadow-xl transition">
+      <Card className="flex flex-col md:flex-row gap-4 p-4 border rounded-xl transition hover:bg-gray-100">
         {post.featured_image && (
-          <div className="w-1/3 relative aspect-[16/9] overflow-hidden rounded-lg">
+          <div className="w-full md:w-1/3 h-40 relative overflow-hidden rounded-lg">
             <Image
               src={post.featured_image}
-              className="w-full h-full"
-              fill
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg"
               alt={post.title}
             />
           </div>
         )}
-        <CardContent className="w-2/3 flex flex-col gap-3">
+        <CardContent className="w-full md:w-2/3 flex flex-col gap-3">
           <div className="flex items-center gap-3 text-sm text-gray-600">
             <Badge variant="secondary">0{sNo}</Badge>
             <span>{Utils.getTimeAgo(post.createdAt)}</span>
           </div>
-          <h2
-            className="text-lg font-semibold hover:text-blue-600 transition"
-          >
+          <h2 className="text-lg font-semibold hover:text-blue-600 transition">
             {post.title}
           </h2>
           <p className="text-gray-500 text-sm">{Utils.textTruncate(post.excerpt, 80)}</p>
